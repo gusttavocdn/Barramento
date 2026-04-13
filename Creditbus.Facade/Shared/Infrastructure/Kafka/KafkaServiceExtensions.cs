adbb3f56d771acf59f25aac8c26f2d310f21d65d
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2026-04-13 00:49:17 -0300

    feat: configura consumer com StoreOffset, AutoCommit e callbacks de partição

--- Creditbus.Facade/Shared/Infrastructure/Kafka/KafkaServiceExtensions.cs
+++ Creditbus.Facade/Shared/Infrastructure/Kafka/KafkaServiceExtensions.cs
@@ -1,6 +1,7 @@
 using Confluent.Kafka;
 using Creditbus.Facade.Features.CardsIngestion.Application.ProcessCardEvent;
 using Creditbus.Facade.Features.CardsIngestion.Infrastructure;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Creditbus.Facade.Shared.Infrastructure.Kafka;
@@ -20,17 +21,53 @@ public static class KafkaServiceExtensions
             return new ProducerBuilder<string, string>(config).Build();
         });
 
+        services.AddSingleton<PartitionWorkerRegistry>();
+
         services.AddSingleton<IConsumer<string, string>>(sp =>
         {
             var options = sp.GetRequiredService<IOptions<KafkaOptions>>().Value;
+            var workerRegistry = sp.GetRequiredService<PartitionWorkerRegistry>();
+            var handlerRegistry = sp.GetRequiredService<KafkaHandlerRegistry>();
+            var dlqPublisher = sp.GetRequiredService<KafkaDlqPublisher>();
+            var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+            var pipeline = KafkaConsumerBackgroundService.BuildPipeline(options.Retry);
+
             var config = new ConsumerConfig
             {
                 BootstrapServers = options.BootstrapServers,
                 GroupId = options.GroupId,
                 AutoOffsetReset = AutoOffsetReset.Earliest,
-                EnableAutoCommit = false
+                EnableAutoCommit = true,
+                AutoCommitIntervalMs = 5000,
+                EnableAutoOffsetStore = false
             };
-            return new ConsumerBuilder<string, string>(config).Build();
+
+            return new ConsumerBuilder<string, string>(config)
+                .SetPartitionsAssignedHandler((consumer, partitions) =>
+                {
+                    foreach (var tp in partitions)
+                    {
+                        var worker = new PartitionWorker(
+                            tp,
+                            consumer,
+                            options.ChannelCapacity,
+                            handlerRegistry,
+                            dlqPublisher,
+                            pipeline,
+                            loggerFactory.CreateLogger<PartitionWorker>());
+                        workerRegistry.Add(tp, worker);
+                        worker.Start();
+                    }
+                })
+                .SetPartitionsRevokedHandler((_, partitions) =>
+                {
+                    foreach (var tp in partitions)
+                    {
+                        var worker = workerRegistry.Remove(tp);
+                        worker?.StopAsync().GetAwaiter().GetResult();
+                    }
+                })
+                .Build();
         });
 
         services.AddScoped<IProcessCardEventUseCase, ProcessCardEventUseCase>();
